8ce7b9ffe27f547533528846a540d5c84ecb3b49
Syed Arshad Ali <ssyarshadali@gmail.com>
2026-08-04 09:36:58 +0500

    Add variables in javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('#grid-container');
+const resizeBtn = document.querySelector('#resize-btn');
+const clearBtn = document.querySelector('#clear-btn');
+const sizeValue = document.querySelector('#size-value');
+const modeButtons = document.querySelectorAll('.mode-btn');
